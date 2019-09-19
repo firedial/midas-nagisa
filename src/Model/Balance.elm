e0c1179d11e95c0e5c97036ec8e97f580962e4ef
firedial <42674772+firedial@users.noreply.github.com>
2019-09-20 00:36:01 +0900

    いい感じに修正

--- src/Model/Balance.elm
+++ src/Model/Balance.elm
@@ -6,74 +6,66 @@ import Json.Encode as Encode exposing (..)
 import Model.Attribute as Attribute
 
 type alias Balance =
-    { amount : Maybe Int
-    , item : Maybe String
-    , kind_id : Maybe Int
-    , purpose_id : Maybe Int
-    , place_id : Maybe Int
-    , date : Maybe String
+    { amount : Int
+    , item : String
+    , kindId : Int
+    , purposeId : Int
+    , placeId : Int
+    , date : String
     }
 
 interpretation : List Attribute.Attribute -> List Attribute.Attribute -> List Attribute.Attribute -> String -> Balance
-interpretation kinds purposes places str =
-    let
-        stringList = String.split " " str |> Maybe.Just
-
-        maybeHead = Maybe.andThen List.head
-        maybeTail = Maybe.andThen List.tail
-        maybeInt = Maybe.andThen String.toInt
-
-        getKindNumber = getAttributeNumber kinds
-        getPurposeNumber = getAttributeNumber purposes
-        getPlaceNumber = getAttributeNumber places
-
-        amount = stringList |> maybeHead |> maybeInt |> maybeMinus
-        item = stringList |> maybeTail |> maybeHead
-        kind_id = stringList |> maybeTail |> maybeTail |> maybeHead |> getKindNumber
-        purpose_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPurposeNumber
-        place_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPlaceNumber
-        date = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead
-    in
-        Balance amount item kind_id purpose_id place_id date
+interpretation kinds purposes places str = init
+-- interpretation kinds purposes places str =
+--     let
+--         stringList = String.split " " str |> Maybe.Just
+-- 
+--         maybeHead = Maybe.andThen List.head
+--         maybeTail = Maybe.andThen List.tail
+--         maybeInt = Maybe.andThen String.toInt
+-- 
+--         getKindNumber = getAttributeNumber kinds
+--         getPurposeNumber = getAttributeNumber purposes
+--         getPlaceNumber = getAttributeNumber places
+-- 
+--         amount = stringList |> maybeHead |> maybeInt |> maybeMinus
+--         item = stringList |> maybeTail |> maybeHead
+--         kind_id = stringList |> maybeTail |> maybeTail |> maybeHead |> getKindNumber
+--         purpose_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPurposeNumber
+--         place_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPlaceNumber
+--         date = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead
+--     in
+--         Balance amount item kind_id purpose_id place_id date
 
 htmlMsg : Balance -> Html msg
 htmlMsg balance =
-    let
-        amount = maybeIntText balance.amount 
-        item = maybeStringText balance.item
-        kind_id = maybeIntText balance.kind_id
-        purpose_id = maybeIntText balance.purpose_id
-        place_id = maybeIntText balance.place_id
-        date = maybeStringText balance.date
-    in  
-        div[]
-            [ text amount
-            , br [] []
-            , text item
-            , br [] []
-            , text kind_id
-            , br [] []
-            , text purpose_id
-            , br [] []
-            , text place_id
-            , br [] []
-            , text date
-            , br [] []
-            ]
+    div[]
+        [ text <| String.fromInt balance.amount
+        , br [] []
+        , text balance.item
+        , br [] []
+        , text <| String.fromInt balance.kindId
+        , br [] []
+        , text <| String.fromInt balance.purposeId
+        , br [] []
+        , text <| String.fromInt balance.placeId
+        , br [] []
+        , text balance.date
+        , br [] []
+        ]
 
 init : Balance
-init =
-    Balance Nothing Nothing Nothing Nothing Nothing Nothing
+init = Balance 0 "" 0 0 0 ""
 
 encode : Balance -> Encode.Value
 encode balance =
     Encode.object
-        [ ("amount", Encode.int <| maybeIntNumber balance.amount)
-        , ("item", Encode.string <| maybeStringText balance.item)
-        , ("kind_id", Encode.int <| maybeIntNumber balance.kind_id)
-        , ("purpose_id", Encode.int <| maybeIntNumber balance.purpose_id)
-        , ("place_id", Encode.int <| maybeIntNumber balance.place_id)
-        , ("date", Encode.string <| maybeStringText balance.date)
+        [ ("amount", Encode.int balance.amount)
+        , ("item", Encode.string balance.item)
+        , ("kind_id", Encode.int balance.kindId)
+        , ("purpose_id", Encode.int balance.purposeId)
+        , ("place_id", Encode.int balance.placeId)
+        , ("date", Encode.string balance.date)
         ]
 
 -- decodeBalance : Decode.Decoder Balance
@@ -86,41 +78,6 @@ encode balance =
 --         (field "place_id" Decode.int)
 --         (field "date" Decode.string)
 
-
-
-
-
-maybeIntNumber : Maybe Int -> Int
-maybeIntNumber n =
-    case n of
-        Nothing ->
-            0
-        Just k ->
-            k
-
-
-maybeIntText : Maybe Int -> String
-maybeIntText n =
-    case n of
-        Nothing ->
-            ""
-        Just k ->
-            String.fromInt k
-
-maybeStringText : Maybe String -> String
-maybeStringText n =
-    case n of
-        Nothing ->
-            ""
-        Just k ->
-            k
-
-maybeMinus : Maybe Int -> Maybe Int
-maybeMinus n =
-    case n of
-        Nothing -> Nothing
-        Just k -> -1 * k |> Maybe.Just
-        
 
 getAttributeNumber : List Attribute.Attribute -> Maybe String -> Maybe Int
 getAttributeNumber attributes str =
--- src/Page/Out.elm
+++ src/Page/Out.elm
@@ -6,19 +6,26 @@ import Html.Events
 import Http 
 import Json.Decode as Decode exposing (..)
 
-import Model.Attribute 
 import Model.Balance
+import Model.Attribute 
+
 
 type alias Model =
     { input : String
     , error : String
+    , balance : Model.Balance.Balance
+    , panel : Panel
     , kinds : List Model.Attribute.Attribute
     , purposes : List Model.Attribute.Attribute
     , places : List Model.Attribute.Attribute
     }
 
 init : ( Model, Cmd Msg )
-init = ( Model "" "" [] [] [], getKindAttributes )
+init = ( Model "" "" Model.Balance.init Amount [] [] [], getAttributes "kind" )
+
+type Direction = Back | Next
+
+type Panel = Amount | Item | Kind | Purpose | Place | Date
 
 type Msg
     = Init
@@ -26,7 +33,8 @@ type Msg
     | Send
     | Receive (Result Http.Error String)
     | GetAttributes (Result Http.Error (List Model.Attribute.Attribute))
-    | AttributeAction String String
+    | AttributeAction Panel String
+    | Panel Direction
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
@@ -37,9 +45,9 @@ update msg model =
             case result of
                 Ok list ->
                     if model.kinds == [] then
-                        ( { model | kinds = list }, getPurposeAttributes)
+                        ( { model | kinds = list }, getAttributes "purpose" )
                     else if model.purposes == [] then
-                        ( { model | purposes = list }, getPlaceAttributes)
+                        ( { model | purposes = list }, getAttributes "place" )
                     else if model.places == [] then
                         ( { model | places = list }, Cmd.none)
                     else
@@ -56,16 +64,14 @@ update msg model =
                     ( { model | error = "err" }, Cmd.none)
         Init ->
             ( { model | error = "init" }, Cmd.none)
-        AttributeAction atr name ->
-            case atr of
-                "kind" ->
-                    ( { model | input = replace 2 model.input name }, Cmd.none)
-                "purpose" ->
-                    ( { model | input = replace 3 model.input name }, Cmd.none)
-                "place" ->
-                    ( { model | input = replace 4 model.input name }, Cmd.none)
-                _ ->
-                    ( model , Cmd.none)
+        AttributeAction panel value ->
+            ( { model | balance = setBalance model.balance model.panel value, panel = getNextPanelName model.panel }, Cmd.none)
+        Panel d ->
+            case d of
+                Back -> 
+                    ( { model | panel = getBackPanelName model.panel }, Cmd.none)
+                Next -> 
+                    ( { model | panel = getNextPanelName model.panel }, Cmd.none)
 
 
 view : Model -> Html Msg
@@ -80,41 +86,17 @@ view model =
         , br [] []
         , text model.error
         , br [] []
-        , Model.Balance.htmlMsg ( Model.Balance.interpretation model.kinds model.purposes model.places model.input )
+        , Model.Balance.htmlMsg model.balance
         , br [] []
         , text model.input
-        , ul [] (List.map ( viewAttributes "kind" ) model.kinds )
         , br [] []
-        , ul [] (List.map ( viewAttributes "purpose" ) model.purposes)
         , br [] []
-        , ul [] (List.map ( viewAttributes "place" ) model.places)
+        , div [] [ button [ Html.Events.onClick ( Panel Back ) ] [ text "back" ] ]
+        , div [] [ button [ Html.Events.onClick ( Panel Next ) ] [ text "next" ] ]
+        , div [] [ getPanelView model ]
         ]
 
 
-
-viewAttributes : String -> Model.Attribute.Attribute -> Html Msg
-viewAttributes name atr =
-    li [] 
-        [ button 
-            [ Html.Attributes.class "aaaa"
-            , Html.Events.onClick ( AttributeAction name atr.name )
-            ]
-            [ text atr.name ]
-        ]
-
-
-
-getKindAttributes : Cmd Msg
-getKindAttributes =
-    getAttributes "kind"
-
-getPurposeAttributes : Cmd Msg
-getPurposeAttributes =
-    getAttributes "purpose"
-
-getPlaceAttributes : Cmd Msg
-getPlaceAttributes =
-    getAttributes "place"
 
 getAttributes : String -> Cmd Msg
 getAttributes attribute =
@@ -136,9 +118,34 @@ decodeAttribute =
         (field "description" Decode.string)
         (field "group_id" Decode.int)
 
+
+
+
+-- Panel
+
+viewAttributes : Panel -> Model.Attribute.Attribute -> Html Msg
+viewAttributes panel atr =
+    li [] 
+        [ button 
+            [ Html.Attributes.class "aaaa"
+            , Html.Events.onClick ( AttributeAction panel ( String.fromInt atr.id ) )
+            ]
+            [ text atr.name ]
+        ]
+
 replace : Int -> String -> String -> String
 replace n input name =
     String.join " " ( replaceLoop n name (String.split " " input) )
+
+setBalance : Model.Balance.Balance -> Panel -> String -> Model.Balance.Balance
+setBalance balance panel value =
+    case panel of
+        Amount -> { balance | amount = getIntFromString value }
+        Item -> { balance | item = value }
+        Kind -> { balance | kindId = getIntFromString value }
+        Purpose -> { balance | purposeId = getIntFromString value }
+        Place -> { balance | placeId = getIntFromString value }
+        Date -> { balance | date = value }
 
 replaceLoop : Int -> String -> List String -> List String
 replaceLoop n name inputList =
@@ -157,6 +164,69 @@ replaceLoop n name inputList =
             (x::xs) -> 
                 x :: ( replaceLoop ( n - 1 ) name xs )
 
+getPanelView : Model -> Html Msg
+getPanelView model =
+    case model.panel of
+        Kind ->
+            ul [] ( List.map ( viewAttributes Kind ) model.kinds )
+        Purpose ->
+            ul [] ( List.map ( viewAttributes Purpose ) model.purposes )
+        Place ->
+            ul [] ( List.map ( viewAttributes Place ) model.places )
+        Date ->
+            div []
+                [ button 
+                    [ Html.Attributes.class "aaaa"
+                    , Html.Events.onClick ( AttributeAction Date "today" )
+                    ]
+                    [ text "today" ]
+                ]
+        _ ->
+            div [] []
+            --     [ Html.form [ Html.Events.onSubmit AttributeAction model.panel ]
+            --         [ input [ Html.Attributes.value model.input ] []
+            --         , button
+            --         [ Html.Attributes.disabled (String.length model.input < 1) ]
+            --         [ text "Submit" ]
+            --     ]
 
+getNextPanelName : Panel -> Panel
+getNextPanelName p =
+    case p of
+        Amount -> Item
+        Item -> Kind
+        Kind -> Purpose
+        Purpose -> Place
+        Place -> Date
+        Date -> Date
+
+getBackPanelName : Panel -> Panel
+getBackPanelName p =
+    case p of
+        Amount -> Amount
+        Item -> Amount
+        Kind -> Item
+        Purpose -> Kind
+        Place -> Purpose
+        Date -> Place
+
+getPanelNumber : Panel -> Int
+getPanelNumber p = 
+    case p of
+        Amount -> 0
+        Item -> 1
+        Kind -> 2
+        Purpose -> 3
+        Place -> 4
+        Date -> 5
+
+getIntFromString : String -> Int
+getIntFromString str =
+    let
+        maybeInt = String.toInt str
+    in
+    case maybeInt of
+        Nothing -> 0
+        Just n -> n
 
 
