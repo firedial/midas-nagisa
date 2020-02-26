6f00c1c3956c16671565519dfeb636df171f04e6
firedial <42674772+firedial@users.noreply.github.com>
2020-02-26 23:05:45 +0900

    リファクタ

--- src/View/Terminal/Out.elm
+++ src/View/Terminal/Out.elm
@@ -4,8 +4,8 @@ import Html exposing (..)
 import Html.Attributes
 import Html.Events
 import Maybe exposing (andThen, withDefault)
-import String exposing (split, toInt)
-import List exposing (head, tail)
+import String exposing (split, join, toInt)
+import List exposing (head, tail, filter, map)
 
 import Model.Balance
 import Model.Attribute as Ma
@@ -16,8 +16,7 @@ getSendAction s = Cmd.none
 getView : Ma.Kinds -> Ma.Purposes -> Ma.Places -> String -> Html msg
 getView kinds purposes places str =
     let
-        balanceString = getBalanceString str
-        balance = getBalanceFromString kinds purposes places balanceString
+        balance = split " " str |> tail |> withDefault [] |> join " " |> getBalanceFromString kinds purposes places
     in
     -- div [] [ text balanceString ]
     Model.Balance.htmlMsg balance
@@ -46,61 +45,10 @@ getBalanceFromString kinds purposes places str =
     in
     Model.Balance.Balance amount item kind purpose place date
 
-maybeListStringToListString : Maybe (List String) -> List String
-maybeListStringToListString strs =
-    case strs of
-        Nothing ->
-            []
-        Just strings ->
-             strings
-
-maybeStringToString : Maybe String -> String 
-maybeStringToString s =
-    case s of
-        Nothing ->
-            ""
-        Just str ->
-            str
-
-maybeStringToInt : Maybe String -> Int
-maybeStringToInt s =
-    case s of
-        Nothing ->
-            0
-        Just str ->
-            stringToInt str
-
-stringToInt : String -> Int
-stringToInt s =
-    let
-        n = String.toInt s 
-    in
-    case n of
-        Nothing ->
-            0
-        Just num ->
-            num
-
-
 getAttributeId : List Ma.Attribute -> String -> Int
 getAttributeId attributes str =
-    let
-        filteredAttributes = List.filter (\n -> n.name == str) attributes
-    in
-        case (List.head filteredAttributes) of
-            Nothing ->
-                0
-            Just a ->
-                a.id
+    filter (\n -> n.name == str) attributes
+    |> map (\n -> n.id)
+    |> head 
+    |> withDefault 0
 
-getBalanceString : String -> String
-getBalanceString str =
-    let
-        balanceStr = List.tail <| String.split " " str
-    in
-    case balanceStr of
-        Nothing ->
-            ""
-        Just s ->
-            String.join " " s
-    
