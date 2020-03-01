46dbf44cf82303579d44edd9d6d82772cc06c6f0
firedial <42674772+firedial@users.noreply.github.com>
2020-03-01 23:31:12 +0900

    いらない部分の削除と微修正

--- src/Model/Balance.elm
+++ src/Model/Balance.elm
@@ -1,4 +1,4 @@
-module Model.Balance exposing (Balance, init, interpretation, encode, htmlMsg)
+module Model.Balance exposing (Balance, init, encode, htmlMsg)
 
 import Html exposing (..)
 import Json.Encode as Encode exposing (..)
@@ -14,43 +14,20 @@ type alias Balance =
     , date : String
     }
 
-interpretation : List Attribute.Attribute -> List Attribute.Attribute -> List Attribute.Attribute -> String -> Balance
-interpretation kinds purposes places str = init
--- interpretation kinds purposes places str =
---     let
---         stringList = String.split " " str |> Maybe.Just
--- 
---         maybeHead = Maybe.andThen List.head
---         maybeTail = Maybe.andThen List.tail
---         maybeInt = Maybe.andThen String.toInt
--- 
---         getKindNumber = getAttributeNumber kinds
---         getPurposeNumber = getAttributeNumber purposes
---         getPlaceNumber = getAttributeNumber places
--- 
---         amount = stringList |> maybeHead |> maybeInt |> maybeMinus
---         item = stringList |> maybeTail |> maybeHead
---         kind_id = stringList |> maybeTail |> maybeTail |> maybeHead |> getKindNumber
---         purpose_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPurposeNumber
---         place_id = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead |> getPlaceNumber
---         date = stringList |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeTail |> maybeHead
---     in
---         Balance amount item kind_id purpose_id place_id date
-
 htmlMsg : Balance -> Html msg
 htmlMsg balance =
     div[]
-        [ text <| String.fromInt balance.amount
+        [ text <| (++) "amount: " <| String.fromInt balance.amount
         , br [] []
-        , text balance.item
+        , text <| (++) "item: " balance.item
         , br [] []
-        , text <| String.fromInt balance.kindId
+        , text <| (++) "kind_id: " <| String.fromInt balance.kindId
         , br [] []
-        , text <| String.fromInt balance.purposeId
+        , text <| (++) "purpose_id: " <| String.fromInt balance.purposeId
         , br [] []
-        , text <| String.fromInt balance.placeId
+        , text <| (++) "place_id: " <| String.fromInt balance.placeId
         , br [] []
-        , text balance.date
+        , text <| (++) "date: " balance.date
         , br [] []
         ]
 
@@ -78,18 +55,3 @@ encode balance =
 --         (field "place_id" Decode.int)
 --         (field "date" Decode.string)
 
-
-getAttributeNumber : List Attribute.Attribute -> Maybe String -> Maybe Int
-getAttributeNumber attributes str =
-    case str of
-        Nothing ->
-            Nothing
-        Just s ->
-            let
-                attribute = List.filter (\n -> n.name == s) attributes
-            in
-                case (List.head attribute) of
-                    Just f ->
-                        Maybe.Just f.id
-                    Nothing ->
-                        Nothing
