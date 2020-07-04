2f1b0faa72432e6cc42757f22a99666689055a64
firedial <42674772+firedial@users.noreply.github.com>
2020-07-05 00:38:43 +0900

    SPA みたいな設計に変えた

--- src/Page/Terminal.elm
+++ src/Page/Terminal.elm
@@ -17,24 +17,22 @@ import View.Terminal.Move
 import Repository.AttributeCollection
 
 type alias Model =
-    { input : String
-    , error : String
-    , balance : Model.Balance.Balance
+    { command : Command
     , acsModel : Repository.AttributeCollection.Model
-    , outModel : View.Terminal.Out.Model
-    , moveModel : View.Terminal.Move.Model
+    , input : String
     }
+
+type Command
+    = None
+    | Out View.Terminal.Out.Model
+    | Move View.Terminal.Move.Model
 
 init : ( Model, Cmd Msg )
 init = 
     let
-        ( outModel, _ ) = View.Terminal.Out.init
-        ( moveModel, _ ) = View.Terminal.Move.init
         ( acsModel, cmd ) = Repository.AttributeCollection.init
     in
-    ( Model "" "" Model.Balance.init acsModel outModel moveModel, Cmd.map GetAttributeCollection cmd )
-
-type Command = None | Out | Move 
+    ( Model None acsModel "" , Cmd.none )
 
 type Msg
     = Send
@@ -47,29 +45,35 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
     case msg of
         Input str ->
-            ( { model | input = str }, Cmd.none )
+            let
+                ( outModel, _ ) = View.Terminal.Out.init
+            in
+            ( { model | input = str, command = Out outModel }, Cmd.none )
         Send ->
-            ( { model | error = "send" }, getCommandSend model)
+            ( model, Cmd.none )
         OutMsg msg_ ->
-            let
-                ( outModel, _ ) = View.Terminal.Out.update msg_ model.outModel
-                input = if outModel.result.msg == "OK" then "" else model.input
-                error = outModel.result.msg
-            in
-            ( { model | outModel = outModel, input = input, error = error }, Cmd.none)
+            case model.command of
+                Out outModel ->
+                    let
+                        ( newModel, newCmd ) = View.Terminal.Out.update msg_ outModel
+                    in
+                    ( { model | command = Out newModel }, Cmd.map OutMsg newCmd)
+                _ ->
+                    ( model, Cmd.none )
         MoveMsg msg_ ->
-            let
-                ( moveModel, _ ) = View.Terminal.Move.update msg_ model.moveModel
-                input = if moveModel.result.msg == "OK" then "" else model.input
-                error = moveModel.result.msg
-            in
-            ( { model | moveModel = moveModel, input = input, error = error }, Cmd.none)
+            case model.command of
+                Move moveModel ->
+                    let
+                        ( newModel, newCmd ) = View.Terminal.Move.update msg_ moveModel
+                    in
+                    ( { model | command = Out newModel }, Cmd.map MoveMsg newCmd)
+                _ ->
+                    ( model, Cmd.none )
         GetAttributeCollection msg_ ->
             let
                 ( attributeCollectionModel, _ ) = Repository.AttributeCollection.update msg_ model.acsModel
             in
             ( { model | acsModel = attributeCollectionModel }, Cmd.none)
-            
 
 view : Model -> Html Msg
 view model =
@@ -87,52 +91,12 @@ view model =
                 ]
             ] 
         , br [] []
-        , div [] [ text model.error ]
-        , div [] [ text model.input ]
-        , div [] [ getCommandPanel model ]
+        , case model.command of
+            None ->
+                Html.text "none"
+            Out outModel ->
+                View.Terminal.Out.view outModel model.acsModel model.input |> Html.map OutMsg
+            Move moveModel ->
+                View.Terminal.Move.view moveModel model.acsModel model.input |> Html.map MoveMsg
         ]
-
-getCommandPanel : Model -> Html msg
-getCommandPanel model = 
-    let
-        command = getCommandName model
-    in
-    case command of
-        None -> div [] [ text "non" ]
-        Out -> View.Terminal.Out.view model.outModel model.acsModel model.input 
-        Move -> View.Terminal.Move.view model.moveModel model.acsModel model.input 
-
-getCommandSend : Model -> Cmd Msg
-getCommandSend model =
-    let
-        command = getCommandName model
-    in
-    case command of
-        None -> Cmd.none
-        Out ->
-            let
-                cmd = View.Terminal.Out.getSendAction model.acsModel model.input 
-            in
-            Cmd.map OutMsg cmd
-        Move ->
-            let
-                cmd = View.Terminal.Move.getSendAction model.acsModel model.input 
-            in
-            Cmd.map MoveMsg cmd
-            
-
-getCommandName : Model -> Command
-getCommandName model =
-    String.split " " model.input
-    |> List.head
-    |> Maybe.andThen getCommandType
-    |> Maybe.withDefault None
-
-getCommandType : String -> Maybe Command
-getCommandType s =
-    case s of
-        "out" -> Just Out
-        "move" -> Just Move
-        _ -> Nothing
-
 
